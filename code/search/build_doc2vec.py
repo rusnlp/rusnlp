d4a31ccdf57c6479e6392b272ee673061ed2e17e
Andrey Kutuzov <akutuzov72@gmail.com>
2018-04-13 19:52:54 +0200

    New doc2vec models

--- code/search/build_doc2vec.py
+++ code/search/build_doc2vec.py
@@ -13,23 +13,21 @@ cores = multiprocessing.cpu_count()
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+    vectorsize = 40
     textdirectory = sys.argv[1]
 
-    files = [f for f in listdir(textdirectory) if f.endswith('.txt')]
+    files = [f for f in listdir(textdirectory) if f.endswith('.conll')]
 
-    texts = [''] * len(files)
+    texts = {}
 
     for doc in files:
         data = open(join(textdirectory, doc)).readlines()
         text = extract_lemmas(data)
-        texts[int(doc.split('.')[0])] = text
+        texts[doc.split('.')[0]] = text
 
-    documents = [gensim.models.doc2vec.TaggedDocument(words=text, tags=[tid]) for tid, text in enumerate(texts)]
-    model = gensim.models.Doc2Vec(documents, vector_size=100, dm=0, dm_mean=1, dbow_words=1,
+    documents = [gensim.models.doc2vec.TaggedDocument(words=texts[text], tags=[text]) for text in texts]
+    model = gensim.models.Doc2Vec(documents, vector_size=vectorsize, dm=0, dm_mean=1, dbow_words=1,
                                   window=5, min_count=2, workers=cores, sample=0, epochs=50)
 
-    # This is all redundant:
-    # model.build_vocab(sentences)
-    # model.train(sentences, total_examples=len(sentences), epochs=10)
-
-    model.save('doc2vec-en.model')
+    filename = 'doc2vec_en_'+str(vectorsize)+'.model'
+    model.save(filename)
--- code/search/use_doc2vec.py
+++ code/search/use_doc2vec.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
 
 import sys
+from os import path
 import logging
 from gensim.models.doc2vec import Doc2Vec
+
+sys.path.insert(0, '../database/')
+from bd import DBaseRusNLP
+from db_reader import ReaderDBase
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 modelfile = sys.argv[1]
 model = Doc2Vec.load(modelfile)
+model.init_sims(replace=True)
 
-user_query = input('Enter a filename to find its nearest neighbors (for example, 80.txt):')
+bd_m = DBaseRusNLP(path.join('..', '..', '..', 'database', 'rus_nlp_withouttexts.db'),
+                   path.join('..', '..', '..', 'database', 'database_metadata.json'))
+reader = ReaderDBase(bd_m)
 
-for i in model.docvecs.most_similar(int(user_query.split('.')[0])):
-    print(i)
+user_query = input('Enter a paper ID to find its nearest neighbors '
+                   '(for example, dialogue_2017_3eac013c2e6d4618fe7308a71e2ef06257ee69db):')
+
+print('Rank\tTitle\tAuthors\tID\tSimilarity')
+sims = model.docvecs.most_similar(user_query)
+
+title = reader.select_title_by_id(user_query)[0][0]
+authors = reader.select_author_by_id(user_query)
+authors = ','.join([w[0] for w in authors])
+rank = 1
+print(str(rank) + '\t' + title + '\t' + authors + '\t' + '1.0')
+print('==========================')
+for sim in sims:
+    doc = sim[0]
+    similarity = sim[1]
+    authors = reader.select_author_by_id(doc)
+    authors = ','.join([w[0] for w in authors])
+    title = reader.select_title_by_id(doc)[0][0]
+    output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+    print(output)
+    rank += 1
