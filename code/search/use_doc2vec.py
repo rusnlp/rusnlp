35307d7842aa932d289536191045133cc2b1491b
akutuzov <akutuzov72@gmail.com>
2018-04-13 23:39:50 +0200

    Batch processing in use_tfidf and use_doc2vec

--- code/search/use_doc2vec.py
+++ code/search/use_doc2vec.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-from os import path
+from os import path, listdir
 import logging
 from gensim.models.doc2vec import Doc2Vec
 
@@ -12,6 +12,7 @@ from db_reader import ReaderDBase
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 modelfile = sys.argv[1]
+testdir = sys.argv[2]
 model = Doc2Vec.load(modelfile)
 model.init_sims(replace=True)
 
@@ -19,24 +20,30 @@ bd_m = DBaseRusNLP(path.join('..', '..', '..', 'database', 'rus_nlp_withouttexts
                    path.join('..', '..', '..', 'database', 'database_metadata.json'))
 reader = ReaderDBase(bd_m)
 
-user_query = input('Enter a paper ID to find its nearest neighbors '
-                   '(for example, dialogue_2017_3eac013c2e6d4618fe7308a71e2ef06257ee69db):')
+# user_query = input('Enter a paper ID to find its nearest neighbors '
+#                    '(for example, dialogue_2017_3eac013c2e6d4618fe7308a71e2ef06257ee69db):')
 
 print('Rank\tTitle\tAuthors\tID\tSimilarity')
-sims = model.docvecs.most_similar(user_query)
+testfiles = [f.split('.')[0] for f in listdir(testdir)]
 
-title = reader.select_title_by_id(user_query)[0][0]
-authors = reader.select_author_by_id(user_query)
-authors = ','.join([w[0] for w in authors])
-rank = 1
-print(str(rank) + '\t' + title + '\t' + authors + '\t' + '1.0')
-print('==========================')
-for sim in sims:
-    doc = sim[0]
-    similarity = sim[1]
-    authors = reader.select_author_by_id(doc)
-    authors = ','.join([w[0] for w in authors])
-    title = reader.select_title_by_id(doc)[0][0]
-    output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
-    print(output)
-    rank += 1
+for user_query in testfiles:
+    sims = model.docvecs.most_similar(user_query)
+
+    title = reader.select_title_by_id(user_query)
+    authors = reader.select_author_by_id(user_query)
+    authors = ','.join([w for w in authors])
+    rank = 0
+    print(str(rank) + '\t' + title + '\t' + authors + '\t' + user_query + '1.0')
+    print('==========================')
+    for sim in sims:
+        rank += 1
+        doc = sim[0]
+        similarity = sim[1]
+        authors = reader.select_author_by_id(doc)
+        authors = ','.join([w for w in authors])
+        title = reader.select_title_by_id(doc)
+        output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+        print(output)
+        rank += 1
+    print('==========================')
+    print('==========================')
--- code/search/use_tfidf.py
+++ code/search/use_tfidf.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-from os import path
+from os import path, listdir
 import json
 import logging
 from gensim import similarities
+
 sys.path.insert(0, '../database/')
 from bd import DBaseRusNLP
 from db_reader import ReaderDBase
@@ -17,35 +18,41 @@ reader = ReaderDBase(bd_m)
 
 orderfile = sys.argv[1]
 indexfile = sys.argv[2]
+testdir = sys.argv[3]
 
 sim_index = similarities.MatrixSimilarity.load(indexfile)
 
 orderdata = open(orderfile, 'r').read()
 order = json.loads(orderdata)
 
-user_query = input('Enter a paper ID to find its nearest neighbors '
-                   '(for example, dialogue_2017_3eac013c2e6d4618fe7308a71e2ef06257ee69db):')
-
-position = order.index(user_query+'.conll')
-
-counter = 0
-for sim in sim_index:
-    if counter == position:
-        similarities = sim
-        break
-    counter += 1
-
-sims = sorted(enumerate(similarities), key=lambda item: -item[1])
 print('Rank\tTitle\tAuthors\tID\tSimilarity')
-rank = 1
-for sim in sims[:11]:
-    doc = order[sim[0]].split('.')[0]
-    similarity = sim[1]
-    authors = reader.select_author_by_id(doc)
-    authors = ','.join([w[0] for w in authors])
-    title = reader.select_title_by_id(doc)[0][0]
-    output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
-    print(output)
-    rank+=1
-    if doc == user_query:
-        print('==========================')
+testfiles = [f.split('.')[0] for f in listdir(testdir) if path.isfile(path.join(testdir, f))]
+
+# user_query = input('Enter a paper ID to find its nearest neighbors '
+#                    '(for example, dialogue_2017_3eac013c2e6d4618fe7308a71e2ef06257ee69db):')
+for user_query in testfiles:
+    print('==========================')
+    position = order.index(user_query + '.conll')
+
+    counter = 0
+    for sim in sim_index:
+        if counter == position:
+            similarities = sim
+            break
+        counter += 1
+
+    sims = sorted(enumerate(similarities), key=lambda item: -item[1])
+
+    rank = 0
+    for sim in sims[:11]:
+        doc = order[sim[0]].split('.')[0]
+        similarity = sim[1]
+        authors = reader.select_author_by_id(doc)
+        authors = ','.join([w for w in authors])
+        title = reader.select_title_by_id(doc)
+        output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+        print(output)
+        rank += 1
+        if doc == user_query:
+            print('==========================')
+    print('==========================')
