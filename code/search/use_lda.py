360cfb163620835528af780058452a76bd10335a
lilaspourpre <irina.nikishina@mail.ru>
2018-04-20 00:56:01 +0300

    use lemmas from pickle for lda search

--- code/search/use_lda.py
+++ code/search/use_lda.py
@@ -35,35 +35,47 @@ corpus = [dictionary.doc2bow(doc) for doc in data]
 index = similarities.MatrixSimilarity(ldamodel[corpus])
 
 
-print('Rank\tTitle\tAuthors\tID\tSimilarity')
-testfiles = [f.split('.')[0] for f in listdir(testdir) if path.isfile(path.join(testdir, f))]
-for name in testfiles:
-    print('==========================')
-    doc = open(path.join(testdir,name+".txt"), 'r', encoding='utf-8').read()
+with open("result_lda_10.tsv", "w", encoding="utf-8") as wri:
+    print('Rank\tTitle\tAuthors\tID\tSimilarity')
+    wri.write('Rank\tTitle\tAuthors\tID\tSimilarity\n')
+    testfiles = [f.split('.')[0] for f in listdir(testdir) if path.isfile(path.join(testdir, f))]
+    for name in testfiles:
+        print('==========================\n')
+        wri.write('==========================\n')
+        wri.write(name+"\n")
+        doc = all_texts_dict[name + '.conll']
+        
+        vec_bow = dictionary.doc2bow(doc)
+        vec_lda = ldamodel[vec_bow]
+        position = order.index(name + '.conll')
     
-    vec_bow = dictionary.doc2bow(doc.split())
-    vec_lda = ldamodel[vec_bow]
-    position = order.index(name + '.conll')
-
-    counter = 0
-    for sim in index:
-        if counter == position:
-            sims = sim
-            break
-        counter += 1
-
-    sims = sorted(enumerate(sims), key=lambda item: -item[1])
-    rank = 0
-    for sim in sims[:11]:
-        doc = order[sim[0]].split('.')[0]
-        print(doc)
-        similarity = sim[1]
-        authors = reader.select_author_by_id(doc)
-        authors = ','.join([w for w in authors])
-        title = reader.select_title_by_id(doc)
-        output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
-        print(output)
-        rank += 1
-        if doc == name:
-            print('==========================')
-    print('==========================')
+        counter = 0
+        for sim in index:
+            if counter == position:
+                sims = sim
+                break
+            counter += 1
+    
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])
+        rank = 0
+        for sim in sims[:11]:
+            doc = order[sim[0]].split('.')[0]
+            print(doc)
+            similarity = sim[1]
+            authors = reader.select_author_by_id(doc)
+            authors = ','.join([w for w in authors])
+            title = reader.select_title_by_id(doc)
+            if doc == name:
+                print('==========================')
+                wri.write('==========================\n')
+                output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+                print(output)
+                wri.write(output+"\n")
+                wri.write('==========================\n')
+            else:
+                output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+                print(output)
+                wri.write(output+"\n")
+            rank += 1
+        print('==========================')
+        wri.write('==========================\n')
