ab9de699c4c0c6206dea47b4a9b6f643f7ed0cd9
Andrey Kutuzov <akutuzov72@gmail.com>
2018-04-20 18:08:50 +0200

    More streamlined use_lda

--- code/search/use_lda.py
+++ code/search/use_lda.py
@@ -3,11 +3,16 @@ from os import path, listdir
 import logging
 from gensim import similarities
 import gensim
-from gensim import corpora
 import pickle
+
 sys.path.insert(0, '../database/')
 from bd import DBaseRusNLP
 from db_reader import ReaderDBase
+
+
+def getpos(ordering, name):
+    return ordering.index(name + '.conll')
+
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -23,59 +28,46 @@ with open(lemmasfile, 'rb') as handle:
     all_texts_dict = pickle.load(handle)
 order = list(all_texts_dict.keys())
 
-def byName_key(topic):
-    return topic[1]
+data = [all_texts_dict[val] for val in order]
 
-
-data = list(all_texts_dict.values())
 ldamodel = gensim.models.ldamodel.LdaModel.load(modelfile)
-dictionary = corpora.Dictionary(data)
-dictionary.filter_n_most_frequent(20)
+dictionary = ldamodel.id2word
+
 corpus = [dictionary.doc2bow(doc) for doc in data]
-index = similarities.MatrixSimilarity(ldamodel[corpus])
+ldacorpus = ldamodel[corpus]
 
+index = similarities.MatrixSimilarity(ldacorpus)
 
-with open("result_lda_10.tsv", "w", encoding="utf-8") as wri:
-    print('Rank\tTitle\tAuthors\tID\tSimilarity')
-    wri.write('Rank\tTitle\tAuthors\tID\tSimilarity\n')
-    testfiles = [f.split('.')[0] for f in listdir(testdir) if path.isfile(path.join(testdir, f))]
-    for name in testfiles:
-        print('==========================\n')
-        wri.write('==========================\n')
-        wri.write(name+"\n")
-        doc = all_texts_dict[name + '.conll']
-        
-        vec_bow = dictionary.doc2bow(doc)
-        vec_lda = ldamodel[vec_bow]
-        position = order.index(name + '.conll')
-    
-        counter = 0
-        for sim in index:
-            if counter == position:
-                sims = sim
-                break
-            counter += 1
-    
-        sims = sorted(enumerate(sims), key=lambda item: -item[1])
-        rank = 0
-        for sim in sims[:11]:
-            doc = order[sim[0]].split('.')[0]
-            print(doc)
-            similarity = sim[1]
-            authors = reader.select_author_by_id(doc)
-            authors = ','.join([w for w in authors])
-            title = reader.select_title_by_id(doc)
-            if doc == name:
-                print('==========================')
-                wri.write('==========================\n')
-                output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
-                print(output)
-                wri.write(output+"\n")
-                wri.write('==========================\n')
-            else:
-                output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
-                print(output)
-                wri.write(output+"\n")
-            rank += 1
-        print('==========================')
-        wri.write('==========================\n')
+print('Rank\tTitle\tAuthors\tID\tSimilarity')
+testfiles = [f.split('.')[0] for f in listdir(testdir) if path.isfile(path.join(testdir, f))]
+
+for name in testfiles:
+    print('==========================\n')
+    print(name)
+    position = getpos(order, name)
+
+    counter = 0
+    for sim in index:
+        if counter == position:
+            sims = sim
+            break
+        counter += 1
+
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])
+    rank = 0
+    for sim in sims[:11]:
+        doc = order[sim[0]].split('.')[0]
+        print(doc)
+        similarity = sim[1]
+        authors = reader.select_author_by_id(doc)
+        authors = ','.join([w for w in authors])
+        title = reader.select_title_by_id(doc)
+        if doc == name:
+            print('==========================')
+            output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+            print(output)
+        else:
+            output = '\t'.join([str(rank), title, authors, doc, str(similarity)])
+            print(output)
+        rank += 1
+    print('==========================')
