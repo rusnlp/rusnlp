4be24130fae0377da0cdbc769d34f052151291d9
lilaspourpre <irina.nikishina@mail.ru>
2020-04-23 03:53:38 +0300

    delete unused files

--- code/filter.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-
-for line in sys.stdin:
-    res = line.strip().split('\t')
-    if len(res) > 0:
-        variants = set(res)
-        print('\t'.join(variants))
--- code/generate_year_subcorpora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in ../../prepared-data/pickles/group_by_data/years/*en.pickle 
-do
-    ./years.py ${i} ../../prepared-data/texts-en-lemma-udpipe > ${i}.txt
-done
-
