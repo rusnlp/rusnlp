09950f6f48efb819342f4f519a8cad8cfabbefdc
PetrFil <50084918+PetrFil@users.noreply.github.com>
2021-03-18 19:31:20 +0300

    authors and affiliations handler

--- code/web/backend/preprocessing/utils/auth&aff_handler.py
+++ code/web/backend/preprocessing/utils/auth&aff_handler.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import pickle
 from transliterate import translit
 from Levenshtein import distance
 
@@ -88,9 +90,37 @@ class AuthorsHandler(ParentHandler):
             my_max += 1
         return tuple(global_res[0])
 
+
+class AffiliationsHandler:
+
+  def __init__(self, aff_tsv: str = 'current_affiliations.tsv', model_path: str = 'aff_classifier.pkl'):
+    """
+    Class for semi-automated affiliations annotation.
+    :param aff_tsv: str, path to affiliations.tsv file
+    :param model_path: str, path to model.pkl file
+    """
+    super().__init__()
+    self.aff_tsv = aff_tsv
+    self.aff_df = pd.read_csv(aff_tsv, sep='\t', index_col=0, names=['aff', 'variation'])
+    self.ind2aff = dict()
+    for ind in self.aff_df.index.unique(): # получаем словарь {index: affiliation}
+      self.ind2aff[ind] = self.aff_df[self.aff_df.index == ind].aff.iloc[0]
+    self.model = pickle.load(open(model_path, 'rb')) # загрузка модели
+
+  def handle_affiliation(self, affiliation):
+      """
+      :param affiliation: affiliation from the article
+      """
+      prediction = self.model.predict([affiliation])
+      return np.vectorize(self.ind2aff.get)(prediction)[0]
+
 # Use case:
 # if __name__=='__main__':
 #     handler = AuthorsHandler()
 #     print(handler.handle_author('Митрофанова О. А.'))
 #     print(handler.handle_author('Елизавета Былинина'))
 #     print(handler.handle_author('Константинова Н.'))
+#     handler = AffiliationsHandler()
+#     print(handler.handle_affiliation('Ломоносова '))
+#     print(handler.handle_affiliation('Московский лингвис'))
+#     print(handler.handle_affiliation('Moscow state'))
