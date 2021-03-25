52edd7afe54cc7a9fdf5d147f69838b16117c7b9
Dmitry Kutsev <dmitrykucev@mail.ru>
2021-03-25 04:00:13 +0300

    input handling added

--- code/web/backend/preprocessing/utils/merge_annotations.py
+++ code/web/backend/preprocessing/utils/merge_annotations.py
@@ -5,25 +5,14 @@ import pandas as pd
 class DoubleError(Exception):
     pass
 
-def merge_annotations():
+def merge_annotations(current_file, new_file, to_csv=False, result_name='my_result.tsv'):
     """
     Merging annotated authors\affilations with previous result.
     It is better to make result name with .tsv format.
     """
     # old_file, new_file, result_name = sys.argv[1:]
 
-    parser = argparse.ArgumentParser()
-    arg = parser.add_argument
-    arg("--current_file", "-c", help="Path to current affiliations or authors", required=True)
-    arg("--new_file", "-n", help="Path to annotated file", required=True)
-    arg("--result_file", "-r", help="Add result file name", required=True)
-
-    args = parser.parse_args()
-    old_file = args.current_file
-    new_file = args.new_file
-    result_name = args.result_file
-
-    old_df = pd.read_csv(old_file, sep='\t', names=['index', 'dict_name', 'name_variants'])
+    old_df = pd.read_csv(current_file, sep='\t', names=['index', 'dict_name', 'name_variants'])
     new_df = pd.read_csv(new_file, sep='\t', names=['text', 'name_variants', 'index', 'dict_name'])
     format_new_df = new_df[['index', 'dict_name', 'name_variants']]
 
@@ -42,5 +31,20 @@ def merge_annotations():
 
 
 if __name__ == '__main__':
-    merge_annotations()
-    # python3 merge_annotations.py current_affiliations.tsv missing_data_missing_affiliations_2021.tsv my_res.tsv
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg("--current_file", "-c", help="Path to current affiliations or authors", required=True)
+    arg("--new_file", "-n", help="Path to annotated file", required=True)
+    arg("--to_csv", "-t", help="You can swith", required=False)
+    arg("--result_file", "-r", help="You can add result file name(path), result.tsv by default", required=False)
+
+    args = parser.parse_args()
+    old_file = args.current_file
+    new_file = args.new_file
+    result_name = args.result_file
+    if new_file and result_name:
+        merge_annotations(old_file, new_file, to_csv=new_file, result_name=result_name)
+    elif new_file and not result_name:
+        merge_annotations(old_file, new_file, to_csv=new_file)
+    else:
+        merge_annotations(old_file, new_file)
