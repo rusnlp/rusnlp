b2e83ccd3c8ee81748567c9e9f3ccc56917d9d2b
Dmitry Kutsev <dmitrykucev@mail.ru>
2021-03-25 03:25:59 +0300

    argparse added

--- code/web/backend/preprocessing/utils/merge_annotations.py
+++ code/web/backend/preprocessing/utils/merge_annotations.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import pandas as pd
 
@@ -9,7 +10,19 @@ def merge_annotations():
     Merging annotated authors\affilations with previous result.
     It is better to make result name with .tsv format.
     """
-    old_file, new_file, result_name = sys.argv[1:]
+    # old_file, new_file, result_name = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg("--current_file", "-c", help="Path to current affiliations or authors", required=True)
+    arg("--new_file", "-n", help="Path to annotated file", required=True)
+    arg("--result_file", "-r", help="Add result file name", required=True)
+
+    args = parser.parse_args()
+    old_file = args.current_file
+    new_file = args.new_file
+    result_name = args.result_file
+
     old_df = pd.read_csv(old_file, sep='\t', names=['index', 'dict_name', 'name_variants'])
     new_df = pd.read_csv(new_file, sep='\t', names=['text', 'name_variants', 'index', 'dict_name'])
     format_new_df = new_df[['index', 'dict_name', 'name_variants']]
