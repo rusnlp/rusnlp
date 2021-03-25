a69ed400f9c518da5b7863276eb816a71bce0e56
Dmitry Kutsev <dmitrykucev@mail.ru>
2021-03-25 03:13:16 +0300

    call from argv added

--- code/web/backend/preprocessing/utils/merge_annotations.py
+++ code/web/backend/preprocessing/utils/merge_annotations.py
@@ -1,13 +1,15 @@
+import sys
 import pandas as pd
 
 class DoubleError(Exception):
     pass
 
-def merge_annotations(old_file, new_file, result_name):
+def merge_annotations():
     """
     Merging annotated authors\affilations with previous result.
     It is better to make result name with .tsv format.
     """
+    old_file, new_file, result_name = sys.argv[1:]
     old_df = pd.read_csv(old_file, sep='\t', names=['index', 'dict_name', 'name_variants'])
     new_df = pd.read_csv(new_file, sep='\t', names=['text', 'name_variants', 'index', 'dict_name'])
     format_new_df = new_df[['index', 'dict_name', 'name_variants']]
@@ -27,8 +29,5 @@ def merge_annotations(old_file, new_file, result_name):
 
 
 if __name__ == '__main__':
-    merge_annotations(
-        'current_affiliations.tsv',
-        'missing_data_missing_affiliations_2021.tsv',
-        'my_res.tsv'
-    )
+    merge_annotations()
+    # python3 merge_annotations.py current_affiliations.tsv missing_data_missing_affiliations_2021.tsv my_res.tsv
